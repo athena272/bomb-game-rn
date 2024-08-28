22efb38733a353c1c53098e339d486c46701554d
athena272 <guilhermera272@gmail.com>
2024-08-28 15:58:12 -0300

    Create style.tsx

---
+++ src/components/Button/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const StyledButton = styled.TouchableOpacity`
+    width: ${RFValue(280)}px;
+    height: ${RFValue(60)}px;
+    background-color: white;
+    margin: ${RFValue(20)}px auto;
+    margin-bottom: 0;
+    border: 1px solid black;
+    border-radius: ${RFValue(10)}px;
+    justify-content: center;
+    align-items: center;
+`;
