aff2bb1d9956f8ec9bd6f9b9285ece2e98d9fcf5
athena272 <guilhermera272@gmail.com>
2024-08-28 15:35:51 -0300

    Create styles.tsx

---
+++ src/pages/Start/styles.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+export const StyledContainer = styled.View`
+    flex: 1;
+    padding-top: ${getStatusBarHeight() + RFValue(50)}px;
+    background-color: #131313;
+`;
+
+export const StyledLogo = styled.Image`
+    width: ${RFValue(270)}px;
+    height: ${RFValue(50)}px;
+    margin: 0 auto;   
+`
+
+export const StyledTitle = styled.Text`
+    margin-top: ${RFValue(70)}px;
+    margin-bottom: ${RFValue(110)}px;
+    font-size: ${RFValue(32)}px;
+    font-family: sans-serif;
+    text-align: center;
+    color: white;
+`;
