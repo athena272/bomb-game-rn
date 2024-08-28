2cbe4f564f751e1b6a731f3f3bc593b823034ec3
athena272 <guilhermera272@gmail.com>
2024-08-28 16:05:08 -0300

    Update index.tsx

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,6 +1,20 @@
-import { StyledContainer, StyledLogo, StyledTitle, StyledSubTitle } from "./styles";
+import { StyledContainer, StyledLogo, StyledTitle, StyledSubTitle, Rules } from "./styles";
+import Button from "../../components/Button";
+import { Alert } from "react-native";
 
 export default function Start() {
+    function handleNavToPlayAlone() {
+        Alert.alert("Botão clicado 1");
+    }
+
+    function handleNavToPlayTogether() {
+        Alert.alert("Botão clicado 2");
+    }
+
+    function handleNavToRules() {
+        Alert.alert("Rules clicado!")
+    }
+
     return (
         <StyledContainer>
             <StyledLogo
@@ -9,6 +23,15 @@ export default function Start() {
             />
             <StyledTitle>Bem-vindo ao {'\n'} Bomb Game</StyledTitle>
             <StyledSubTitle>Escolha um modo de jogo.</StyledSubTitle>
+            <Button
+                buttonText={"Jogar Solo"}
+                handlePress={handleNavToPlayAlone}
+            />
+            <Button
+                buttonText={"Jogar Em Dupla"}
+                handlePress={handleNavToPlayTogether}
+            />
+            <Rules onPress={handleNavToRules}>Ver as regras do jogo</Rules>
         </StyledContainer>
     );
 }
