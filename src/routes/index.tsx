22e413a494c564e2740d148c05184565adccdc9c
athena272 <guilhermera272@gmail.com>
2024-08-28 22:27:30 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../types";
 import Start from "../pages/Start";
 import Rules from "../pages/Rules";
+import PlayAlone from "../pages/PlayAlone";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -12,6 +13,7 @@ export default function Routes() {
             <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="Rules" component={Rules} />
+                <Stack.Screen name="PlayAlone" component={PlayAlone} />
             </Stack.Navigator>
         </NavigationContainer>
     );
