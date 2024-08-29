f16a6516b6712d851238a6241bbc272a6358a757
athena272 <guilhermera272@gmail.com>
2024-08-29 08:46:38 -0300

    Update index.tsx

--- src/pages/PlayAlone/index.tsx
+++ src/pages/PlayAlone/index.tsx
@@ -1,9 +1,22 @@
 import { StyledContainer, StyledTitle } from "./styles";
+import { ImageBackground } from "react-native";
+import bombImg from '../../assets/bomba.png'
 
 export default function PlayAlone() {
     return (
         <StyledContainer>
-            <StyledTitle>Bomb Hame Solo</StyledTitle>
+            <StyledTitle>Bomb Game Solo</StyledTitle>
+            <ImageBackground
+                source={bombImg}
+                resizeMode="cover"
+                style={{
+                    marginTop: 50,
+                    minHeight: 130,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+            </ImageBackground>
         </StyledContainer>
     )
 }
