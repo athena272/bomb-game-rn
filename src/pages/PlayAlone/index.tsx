135d5e5e0f3500fb631f67df14c0258da19fc9ba
athena272 <guilhermera272@gmail.com>
2024-08-28 22:27:37 -0300

    Create index.tsx

---
+++ src/pages/PlayAlone/index.tsx
@@ -0,0 +1,9 @@
+import { StyledContainer, StyledTitle } from "./styles";
+
+export default function PlayAlone() {
+    return (
+        <StyledContainer>
+            <StyledTitle>Bomb Hame Solo</StyledTitle>
+        </StyledContainer>
+    )
+}
