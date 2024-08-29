532d644f4f09449a82482d9809e8fd3f587d4d38
athena272 <guilhermera272@gmail.com>
2024-08-28 21:55:37 -0300

    add theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,43 @@
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { useCallback, useEffect } from "react";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import Routes from "./src/routes";
+import theme from "./src/global/styles/theme";
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <Text>Loading</Text>;
+  }
+
   return (
-    <Routes />
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </View>
   );
 }
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -5,5 +5,10 @@ export default {
         background: "#131313",
         textLight: "#bbb",
         explodeBackground: "#ff0044",
-    }
+    },
+    fonts: {
+        regular: "Roboto_400Regular",
+        medium: "Roboto_500Medium",
+        bold: "Roboto_700Bold",
+    },
 };
