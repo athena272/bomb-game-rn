d9edf45ed27b12479962c3a38c2f2f65ea70b4a8
athena272 <guilhermera272@gmail.com>
2024-08-30 16:43:25 -0300

    Update index.tsx

--- src/pages/Disarmed/index.tsx
+++ src/pages/Disarmed/index.tsx
@@ -1,10 +1,26 @@
-import { Logo, StyledContainer } from "./styles"
+import { NavigationProp, useNavigation } from "@react-navigation/native"
+import { RootStackParamList } from "../../types"
+import { Logo, StyledContainer, StyledTitle, SucessImg } from "./styles"
 import logoImg from '../../assets/logoDark.png'
+import successImg from '../../assets/bomba_cortada_matrix.png'
+import Button from "../../components/Button"
 
 export default function Disarmed() {
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>()
+
+    function handleNavToStart() {
+        navigation.navigate('Start')
+    }
+
     return (
         <StyledContainer>
             <Logo source={logoImg} style={{ resizeMode: 'contain' }} />
+            <StyledTitle>Parabéns!!!{"\n"}Você desarmou a bomba</StyledTitle>
+            <SucessImg source={successImg} style={{ resizeMode: 'contain' }} />
+            <Button
+                buttonText="Página incial"
+                handlePress={handleNavToStart}
+            />
         </StyledContainer>
     )
 }
