7b5a14f72b24e03f69c473a90003ee5846a72e90
athena272 <guilhermera272@gmail.com>
2024-08-30 14:04:39 -0300

    Create styles.tsx

---
+++ src/components/PlayTogether/PasswordInput/styles.tsx
@@ -0,0 +1,36 @@
+import { TextInput, TextInputProps } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import React from "react";
+
+interface StyledInputProps extends TextInputProps {
+    abled?: boolean
+}
+
+export const StyledContainer = styled.View`
+    margin: ${RFValue(50)}px ${RFValue(30)}px;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const InputContainer = styled.View`
+    border: ${RFValue(1)}px solid white;
+    border-radius: ${RFValue(5)}px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledInput = styled(
+    React.forwardRef<TextInput, StyledInputProps>((props, ref) => (
+        <TextInput ref={ref} {...props} />
+    ))
+)`
+    font-size: ${RFValue(30)}px;
+    padding: ${RFValue(10)}px;
+    color: ${({ theme }) => theme.colors.white};
+    text-align: center;
+    border-radius: ${RFValue(5)}px;
+    width: 100%;
+    background-color: ${(props) => props.abled === false ? "#9999998c" : "transparent"};
+`;    
