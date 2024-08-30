6d1ce29f8b96f677826947decdbda0439aca9c28
athena272 <guilhermera272@gmail.com>
2024-08-30 13:18:50 -0300

    Update styles.tsx

--- src/pages/PlayTogether/TipInput/styles.tsx
+++ src/pages/PlayTogether/TipInput/styles.tsx
@@ -2,33 +2,30 @@ import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const StyledContainer = styled.View`
-    margin: 0 ${RFValue(45)}px;
+  margin: 0 ${RFValue(45)}px;
 `;
 
 export const TipTitle = styled.Text`
-    font-size: ${RFValue(24)}px;
-    font-family: ${({ theme }) => theme.fonts.bold};
-    color: ${({ theme }) => theme.colors.white};
-    text-decoration-line: underline;
-    text-align: center;
-    border: 1px solid red;
+  font-size: ${RFValue(24)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
 `;
 
 export const InputContainer = styled.View`
-    width: ${RFValue(260)}px;
-    height: ${RFValue(30)}px;
-    border: ${RFValue(1)}px solid white;
-    border-radius: ${RFValue(5)}px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: ${RFValue(260)}px;
+  height: ${RFValue(30)}px;
+  border: ${RFValue(1)}px solid white;
+  border-radius: ${RFValue(5)}px;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const StyledInput = styled.TextInput.attrs({
     placeholderTextColor: "white",
 })`
-    width: 90%;
-    font-size: ${RFValue(12)}px;
-    color: white;
-    font-family: ${({ theme }) => theme.fonts.regular};
+  width: 90%;
+  font-size: ${RFValue(12)}px;
+  color: white;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
