69a75c7dc9ed3124c09ef576dbe3edcb3dbd9427
athena272 <guilhermera272@gmail.com>
2024-08-31 00:56:19 -0300

    Update index.tsx

--- src/pages/PlayAlone/index.tsx
+++ src/pages/PlayAlone/index.tsx
@@ -1,37 +1,88 @@
-import { useState } from "react";
-import { Alert, ImageBackground } from "react-native";
+import { useEffect, useState } from "react";
+import { ImageBackground } from "react-native";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import bombImg from '../../assets/bomba.png'
 import { StyledContainer, StyledTitle, TextTimer, Timer, TipContainer, TipText, TipTitle } from "./styles";
-import { RootStackParamList } from "../../types";
+import { RootStackParamList, ApiResponse, Question } from "../../types";
 import PasswordInput from "../../components/PasswordInput";
 import Button from "../../components/Button";
 import BombService from "../../services/BombApp";
+import api from "../../services/api/api";
 
 export default function PlayAlone() {
     const [started, setStarted] = useState(false);
     const [pin, setPin] = useState(["", "", ""]);
     const [hours, setHours] = useState("00");
-    const [minutes, setMinutes] = useState("03");
-    const [seconds, setSeconds] = useState("00");
-
-    function handleStartBomb() {
-        const diffTime = BombService.getDiffTime({ hours, seconds, minutes })
-    }
-
+    const [minutes, setMinutes] = useState("00");
+    const [seconds, setSeconds] = useState("05");
     const [question, setQuestion] = useState("");
     const [answer, setAnswer] = useState("");
-    const [intervalId, setIntervalId] = useState();
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
 
+    // Controla o inicio de jogo
+    function handleStartGame() {
+        BombService.bombStartGame({ setStarted, hours, minutes, seconds })
+    }
+
+    // Controla a bomba
+    function handleStartBomb() {
+        const diffTime = BombService.getDiffTime({ hours, seconds, minutes })
+
+        BombService.startCountdown({
+            setSeconds,
+            setMinutes,
+            setHours,
+            setStarted,
+            diffTime,
+            setIntervalId,
+            intervalId,
+            navigation,
+        })
+    }
+
+    //Controla o disarme da bomba
+    function handleDisarmBomb() {
+        BombService.disarmBomb({
+            setStarted,
+            answer,
+            navigation,
+            pin,
+            setPin,
+            intervalId,
+        })
+    }
+
+    // manipular as questões e respostas
+    async function fetchQuestion() {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+        const { data } = await api.get<ApiResponse>("questions.json");
+        // console.log("🚀 ~ fetchQuestion ~ data:", data)
+
+        const randomQuestion = data.questions.find(question => question.id === randomNumber)
+
+        if (randomQuestion) {
+            setQuestion(randomQuestion?.pergunta)
+            setAnswer(randomQuestion?.resp)
+        }
+    }
+
+    // Controle de navegação na tela
     const navigation = useNavigation<NavigationProp<RootStackParamList>>()
 
     function handleNavToStart() {
         navigation.navigate('Start')
     }
 
-    function handleStartGame() {
-        Alert.alert('Jogo começou!')
-    }
+    useEffect(() => {
+        if (started) {
+            handleStartBomb();
+        }
+    }, [started]);
+
+    useEffect(() => {
+        fetchQuestion();
+    }, []);
 
     return (
         <StyledContainer>
@@ -48,18 +99,25 @@ export default function PlayAlone() {
             >
                 <Timer>
                     <TextTimer>
-                        {'00'} : {'05'} : {'00'}
+                        {hours} : {minutes} : {seconds}
                     </TextTimer>
                 </Timer>
             </ImageBackground>
 
-            <TipContainer>
-                <TipTitle>Sua dica:</TipTitle>
-                <TipText>Dica vai estar aqui!</TipText>
-            </TipContainer>
+            {!started ? null : (
+                <TipContainer>
+                    <TipTitle>Sua dica:</TipTitle>
+                    <TipText>{question}</TipText>
+                </TipContainer>
+            )}
+            <TipText>{answer}</TipText>
 
-            <PasswordInput />
-            <Button buttonText="Iniciar" handlePress={handleStartGame} />
+            <PasswordInput pin={pin} setPin={setPin} />
+            {!started ? (
+                <Button buttonText="Iniciar" handlePress={handleStartGame} />
+            ) : (
+                <Button buttonText="Desarmar" handlePress={handleDisarmBomb} />
+            )}
             <Button buttonText="Página Inicial" handlePress={handleNavToStart} />
         </StyledContainer>
     )
